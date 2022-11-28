f726d887300cb058a02622c658c8a5d9e99dfb20
Владимир  Петров <petrov@vgapo.int>
2022-11-28 17:06:01 +0300

    решение 1 задачи другим способом

---
+++ DZ2/z2.1_1.py
@@ -0,0 +1,13 @@
+# 1. Напишите программу, которая принимает на вход
+# вещественное число и показывает сумму его цифр.
+# Без работы с методами строк.
+
+
+from decimal import Decimal
+num = abs(Decimal(input("Введите число: "))).as_tuple()
+
+print("Цифры числа\n", num.digits)
+sum = 0
+for digit in num.digits:
+    sum += digit
+print(sum)
